e0c1206feeb932a005f5be7a3e83b6172c079acf
kimiya <kimia.beheshti@tum.de>
2018-08-30 02:30:26 +0200

    get users data/http post reques

---
+++ src/App.vue
@@ -0,0 +1,30 @@
+<template>
+  <div>
+    <show-blog></show-blog>
+  </div>
+</template>
+
+<script>
+  // Imports
+  import addBlog from './components/addBlog.vue';
+  import showBlog from './components/showBlog.vue';
+  export default {
+    components: {
+      'add-blog': addBlog,
+      'show-blog': showBlog
+    },
+    data () {
+      return {
+      }
+    },
+    methods: {
+    }
+  }
+</script>
+
+<style>
+  body{
+    margin: 0;
+    font-family: 'Nunito SemiBold';
+  }
+</style>
---
+++ src/components/showBlog.vue
@@ -0,0 +1,37 @@
+<template>
+  <div>
+    This is the list of all blogs.
+    <div v-for="blog in blogs">
+      <h2>{{blog.title}}</h2>
+      <article>{{blog.body}}</article>
+    </div>
+  </div>
+</template>
+
+<script>
+  export default {
+
+    data () {
+      return {
+        blogs:[]
+      }
+    },
+    methods: {
+    },
+    created() {
+        this.$http.get('https://jsonplaceholder.typicode.com/posts').then(function (data) {
+          this.blogs=data.body.slice(0-10);
+        })
+      }
+
+
+  }
+
+</script>
+
+<style>
+  body{
+    margin: 0;
+    font-family: 'Nunito SemiBold';
+  }
+</style>
